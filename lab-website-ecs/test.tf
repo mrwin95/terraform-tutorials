c050d8366f66be838ea7ac1a8f0b40e36b9595e1
Thang Nguyen <mrwin95@gmail.com>
2024-07-25 13:37:26 -0600

    completed create a natgateway

--- lab-website-ecs/test.tf
+++ lab-website-ecs/test.tf
@@ -18,3 +18,15 @@ module "vpc" {
   private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
   private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
 }
+
+module "nat_gateway" {
+  source                     = "../certificate/modules/nat-gateway"
+  public_subnet_az1_id       = module.vpc.public_subnet_az1_id
+  public_subnet_az2_id       = module.vpc.public_subnet_az2_id
+  private_app_subnet_az1_id  = module.vpc.private_app_subnet_az1_id
+  private_app_subnet_az2_id  = module.vpc.private_app_subnet_az2_id
+  private_data_subnet_az1_id = module.vpc.private_data_subnet_az1_id
+  private_data_subnet_az2_id = module.vpc.private_data_subnet_az2_id
+  vpc_id                     = module.vpc.vpc_id
+  aws_internet_gateway       = module.vpc.aws_internet_gateway
+}
